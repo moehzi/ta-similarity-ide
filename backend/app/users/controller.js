39403098a8737cba551d619061db3d9980673819
moehzi <faizi1234098@gmail.com>
2022-05-21 21:13:22 +0800

    feat: add login api

--- backend/app/users/controller.js
+++ backend/app/users/controller.js
@@ -1,5 +1,19 @@
 const User = require('./model');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const {
+  JWT_SIGNATURE_KEY = '$2b$10$o/Um9jZOMZxohkDcSpmd7.2DPVB1GLYOo04rQdC3tc81WT2jzYTem',
+} = process.env;
+
+function isPasswordValid(password, encryptedPassword) {
+  return bcrypt.compareSync(password, encryptedPassword);
+}
+
+function createToken(payload) {
+  return jwt.sign(payload, JWT_SIGNATURE_KEY, {
+    expiresIn: '1h',
+  });
+}
 
 module.exports = {
   index: async (req, res) => {
@@ -47,6 +61,47 @@ module.exports = {
         },
       });
     } catch (err) {
+      res.send({ message: err.message || 'Internal Server Errror' });
+    }
+  },
+
+  actionLogin: async (req, res) => {
+    try {
+      const { username, password } = req.body;
+
+      const user = await User.findOne({ NIM: username });
+
+      if (!user) {
+        return res.status(401).json({
+          status: 'FAIL',
+          data: {
+            name: 'UNAUTHORIZED',
+            message: 'Username does not exist',
+          },
+        });
+      }
+
+      if (!isPasswordValid(password, user.encryptedPassword)) {
+        return res.status(401).json({
+          status: 'FAIL',
+          data: {
+            name: 'UNAUTHORIZED',
+            message: 'Wrong password',
+          },
+        });
+      }
+
+      return res.status(201).json({
+        status: 'OK',
+        data: {
+          token: createToken({
+            id: user.id,
+            username: user.NIM,
+            role: user.role,
+          }),
+        },
+      });
+    } catch (error) {
       res.send({ message: err.message || 'Internal Server Errror' });
     }
   },
--- backend/app/users/router.js
+++ backend/app/users/router.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const { index, actionRegister } = require('./controller');
+const { index, actionRegister, actionLogin } = require('./controller');
 
 /* GET home page. */
 router.get('/', index);
 router.post('/register', actionRegister);
+router.post('/login', actionLogin);
 module.exports = router;
