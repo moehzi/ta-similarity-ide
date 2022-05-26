305fade563a73f795f245da6b4aaa96a1d5d60bd
moehzi <faizi1234098@gmail.com>
2022-05-26 19:59:13 +0800

    test: update title

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const authRouter = require('./app/auth/router');
 
 var app = express();
 
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World</h1>');
+});
 // view engine setup
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
