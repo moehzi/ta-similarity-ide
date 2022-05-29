2db3aba9eb2a994ad6e1a4409ec5198bb04f49e5
Muhammad Nur Faisi S <faizi1234098@gmail.com>
2022-05-29 20:03:50 +0800

    feat: add myprofile feature

--- app/users/controller.js
+++ app/users/controller.js
@@ -9,4 +9,24 @@ module.exports = {
       data: user,
     });
   },
+
+  myProfile: async (req, res) => {
+    const user = await User.findOne({ _id: req.user.id });
+
+    if (!user)
+      return res.status(404).json({
+        status: 'Fail',
+        message: 'Not Found',
+      });
+
+    return res.status(200).json({
+      status: 'OK',
+      data: {
+        username: user.registrationNumber,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+    });
+  },
 };
--- app/users/router.js
+++ app/users/router.js
@@ -2,8 +2,9 @@ var express = require('express');
 const isAuth = require('../../middlewares/isAuth');
 const isRole = require('../../middlewares/isRole');
 var router = express.Router();
-const { getUsers } = require('./controller');
+const { getUsers, myProfile } = require('./controller');
 
 /* GET home page. */
 router.get('/users', isAuth, isRole(['teacher']), getUsers);
+router.get('/profile', isAuth, isRole(['teacher', 'student']), myProfile);
 module.exports = router;
