43c984dfc06915e47edd2ac5f2f9eb6c560454b5
moehzi <faizi1234098@gmail.com>
2022-11-10 02:37:25 +0800

    feat: add role when register

--- app/auth/router.js
+++ app/auth/router.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
 const { actionRegister, actionLogin } = require('./controller');
-
+const isRole = require('../../middlewares/isRole');
+const isAuth = require('../../middlewares/isAuth');
 /* GET home page. */
-router.post('/register', actionRegister);
+router.post('/register', isAuth, isRole(['teacher']), actionRegister);
 router.post('/login', actionLogin);
 module.exports = router;
