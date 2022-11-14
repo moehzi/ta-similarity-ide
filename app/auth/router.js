e66244b2ed4e83b6c1089267bdf0830f07264d89
moehzi <faizi1234098@gmail.com>
2022-11-14 15:57:13 +0800

    fix: fix register

--- app/auth/router.js
+++ app/auth/router.js
@@ -4,6 +4,6 @@ const { actionRegister, actionLogin } = require('./controller');
 const isRole = require('../../middlewares/isRole');
 const isAuth = require('../../middlewares/isAuth');
 /* GET home page. */
-router.post('/register', isAuth, isRole(['teacher']), actionRegister);
+router.post('/register', actionRegister);
 router.post('/login', actionLogin);
 module.exports = router;
--- app/users/router.js
+++ app/users/router.js
@@ -7,5 +7,5 @@ const { getUsers, myProfile, getTeacher } = require('./controller');
 /* GET home page. */
 router.get('/users', isAuth, isRole(['teacher']), getUsers);
 router.get('/profile', isAuth, isRole(['teacher', 'student']), myProfile);
-router.get('/user/teacher', isAuth, isRole(['teacher']), getTeacher);
+router.get('/user/teacher', isAuth, isRole(['teacher', 'student']), getTeacher);
 module.exports = router;
