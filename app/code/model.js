53f14000c4162dd385070e5881015b8b703cea23
moehzi <faizi1234098@gmail.com>
2022-11-17 11:39:05 +0800

    add timestamp

--- app/code/model.js
+++ app/code/model.js
@@ -1,32 +1,35 @@
 const mongoose = require('mongoose');
-const codeSchema = mongoose.Schema({
-  jsCode: {
-    type: String,
-    default: '',
+const codeSchema = mongoose.Schema(
+  {
+    jsCode: {
+      type: String,
+      default: '',
+    },
+    htmlCode: {
+      type: String,
+      default: '',
+    },
+    cssCode: {
+      type: String,
+      default: '',
+    },
+    author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    workId: { type: mongoose.Schema.Types.ObjectId, ref: 'Work' },
+    status: {
+      type: String,
+      required: [true, 'Status tidak boleh kosong'],
+      default: 'Not Completed',
+    },
+    highestPercentage: {
+      type: String,
+      default: '',
+    },
+    esprimaCode: { type: String, default: '' },
+    similarityResult: [],
+    classId: { type: mongoose.Schema.Types.ObjectId, ref: 'Class' },
+    courseId: { type: mongoose.Schema.Types.ObjectId, ref: 'Course' },
   },
-  htmlCode: {
-    type: String,
-    default: '',
-  },
-  cssCode: {
-    type: String,
-    default: '',
-  },
-  author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  workId: { type: mongoose.Schema.Types.ObjectId, ref: 'Work' },
-  status: {
-    type: String,
-    required: [true, 'Status tidak boleh kosong'],
-    default: 'Not Completed',
-  },
-  highestPercentage: {
-    type: String,
-    default: '',
-  },
-  esprimaCode: { type: String, default: '' },
-  similarityResult: [],
-  classId: { type: mongoose.Schema.Types.ObjectId, ref: 'Class' },
-  courseId: { type: mongoose.Schema.Types.ObjectId, ref: 'Course' },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('Code', codeSchema);
--- app/works/model.js
+++ app/works/model.js
@@ -1,32 +1,35 @@
 const mongoose = require('mongoose');
 
-const workSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'Nama tidak boleh kosong'],
+const workSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Nama tidak boleh kosong'],
+    },
+    description: {
+      type: String,
+      required: [true, 'Deskripsi tidak boleh kosong'],
+    },
+    codeTest: {
+      type: String,
+      required: [true, 'Code Test tidak boleh kosong'],
+    },
+    htmlStarter: { type: String },
+    cssStarter: { type: String },
+    jsStarter: { type: String },
+    code: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Code' }],
+    status: {
+      type: String,
+      default: 'Not ready to review',
+    },
+    algorithmSimilarity: { type: String },
+    deadline: { type: Number, required: [true, 'Deadline tidak boleh kosong'] },
+    classId: { type: mongoose.Schema.Types.ObjectId, ref: 'Class' },
+    exerciseId: { type: mongoose.Schema.Types.ObjectId },
+    courseId: { type: mongoose.Schema.Types.ObjectId, ref: 'Class' },
+    isVisible: { type: Boolean, default: true },
   },
-  description: {
-    type: String,
-    required: [true, 'Deskripsi tidak boleh kosong'],
-  },
-  codeTest: {
-    type: String,
-    required: [true, 'Code Test tidak boleh kosong'],
-  },
-  htmlStarter: { type: String },
-  cssStarter: { type: String },
-  jsStarter: { type: String },
-  code: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Code' }],
-  status: {
-    type: String,
-    default: 'Not ready to review',
-  },
-  algorithmSimilarity: { type: String },
-  deadline: { type: Number, required: [true, 'Deadline tidak boleh kosong'] },
-  classId: { type: mongoose.Schema.Types.ObjectId, ref: 'Class' },
-  exerciseId: { type: mongoose.Schema.Types.ObjectId },
-  courseId: { type: mongoose.Schema.Types.ObjectId, ref: 'Class' },
-  isVisible: { type: Boolean, default: true },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('Work', workSchema);
